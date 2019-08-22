8413d9f05b744390777ad671ee0f2614158a2546
Stanley Huang <huang.stanley050@gmail.com>
2019-08-22 21:00:20 +1000

    finished the navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import PersonIcon from "@material-ui/icons/Person";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1)
+  },
+  leftIcon: {
+    marginRight: theme.spacing(1)
+  },
+  rightIcon: {
+    marginLeft: theme.spacing(1)
+  },
+  iconSmall: {
+    fontSize: 20
+  },
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            File Uploader
+          </Typography>
+          <Button variant="contained" className={classes.button}>
+            <CloudUploadIcon />
+            Upload
+          </Button>
+          <Button variant="contained" className={classes.button}>
+            <PersonIcon />
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
