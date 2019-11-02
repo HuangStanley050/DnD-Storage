398a97c5fa6a5ede9a6d7da19d00e37c7210b61a
Stanley Huang <huang.stanley050@gmail.com>
2019-11-02 17:05:03 +1100

    changed api end point to herokus

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 export default {
-  login: "http://localhost:8000/api/auth/login",
-  upload: "http://localhost:8000/api/data/datastore",
-  download: "http://localhost:8000/api/data/datastore/download/",
-  delete: "http://localhost:8000/api/data/datastore"
-  //upload and data fetch are the same, differs in GET and POST request
+  login: "https://dnd-react-api.herokuapp.com/api/auth/login",
+  upload: "https://dnd-react-api.herokuapp.com/api/data/datastore",
+  download: "https://dnd-react-api.herokuapp.com/api/data/datastore/download/",
+  delete: "https://dnd-react-api.herokuapp.com/api/data/datastore"
+  // upload and data fetch are the same, differs in GET and POST request
 };
