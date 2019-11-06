e8dedb7103df91cfca4156c9470cadb92a3bf2f1
Stanley Huang <huang.stanley050@gmail.com>
2019-11-06 20:43:11 +1100

    able to test nested material ui component with 'mount'

--- src/__tests__/components/Login.test.js
+++ src/__tests__/components/Login.test.js
@@ -1,20 +1,51 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
-import { shallow, mount, render } from "../../enzyme";
+import { createMount, createShallow } from "@material-ui/core/test-utils";
+import { render } from "../../enzyme";
 import { Login } from "../../components/Login";
 
-describe("Login test render", () => {
+describe("<Login /> test render", () => {
   let wrapper;
+  let mount;
+  let shallow;
+  let login;
   beforeEach(() => {
-    const login = jest.fn();
+    login = jest.fn();
+    mount = createMount();
+    shallow = createShallow();
+  });
+  afterEach(() => {
+    mount.cleanUp();
+  });
+  it("should render the form without error", () => {
     wrapper = shallow(
       <Login isAuth={false} login={login} loading={false} error="" />
     );
-  });
-  it("should render the form without error", () => {
     expect(wrapper.exists()).toBe(true);
   });
   it("should render two input fields for login form", () => {
+    wrapper = mount(
+      <Login isAuth={false} login={login} loading={false} error="" />
+    );
     expect(wrapper.find(TextField)).toHaveLength(2);
+  });
+  it("should render one input field for password, one for email", () => {
+    wrapper = mount(
+      <Login isAuth={false} login={login} loading={false} error="" />
+    );
+    // wrapper.find(`[data-test="${val}"]`)
+
+    expect(
+      wrapper
+        .find("input")
+        .at(0)
+        .props().name
+    ).toBe("email");
+    expect(
+      wrapper
+        .find("input")
+        .at(1)
+        .props().name
+    ).toBe("password");
   });
 });
