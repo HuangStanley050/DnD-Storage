58531eaee35714553680bbafafd4cc240faf588c
Stanley Huang <huang.stanley050@gmail.com>
2019-11-08 10:28:34 +1100

    landing page component done

---
+++ src/__tests__/components/Landing.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { render, mount, shallow } from "../../enzyme";
+import Landing from "../../components/Landing";
+
+describe("<Landing /> should render without failure", () => {
+  it("should render a landing page", () => {
+    const wrapper = shallow(<Landing />);
+    expect(wrapper.exists()).toBe(true);
+  });
+  it("should render text of Landing Page", () => {
+    const wrapper = shallow(<Landing />);
+    expect(wrapper.text()).toBe("Landing Page");
+  });
+});
